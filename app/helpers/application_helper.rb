3183dc41516a536c6f207749af045ae2a3afbb87
da <da@localhost.localdomain>
2019-10-02 11:29:48 +0300

    Add plural to finding title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def get_profile_value(val)
     t("profiles.#{val}")
   end
+
+  def plural(count, value, locale = I18n.locale)
+    val = "plural.#{value}"
+    count.to_s + ' ' + Russian.p(count, t("#{val}_1", locale: locale), t("#{val}_2", locale: locale), t("#{val}_10", locale: locale))
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  it "with count equal 1" do
-    expect(helper.color_name('red')).to eq("красный")
+  it "get profile value mus return localized val" do
+    expect(helper.get_profile_value('red')).to eq("красный")
+  end
+
+  describe "check plural" do
+    it "with count equal 1" do
+      expect(helper.plural(1, 'master')).to eq("1 мастер")
+    end
+
+    it "with count equal 2" do
+      expect(helper.plural(2, 'master')).to eq("2 мастера")
+    end
+
+    it "with count equal 10" do
+      expect(helper.plural(10, 'master')).to eq("10 мастеров")
+    end
   end
 end

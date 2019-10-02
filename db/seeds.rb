1ac85e72238f177b28170c24d50830f203f5043d
da <da@localhost.localdomain>
2019-10-02 08:26:53 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+password = 'qwerty444'
+
+elena = User.create!(username: 'Елена Сергиевич',   email: 'elena@tatoojmb.info', password: password, password_confirmation: password)
+angel = User.create!(username: 'Ангелина Терехова', email: 'angel@tatoojmb.info', password: password, password_confirmation: password)
+ivans = User.create!(username: 'Иван Сидоров',      email: 'ivans@tatoojmb.info', password: password, password_confirmation: password)
+aleks = User.create!(username: 'Алексей Стриженов', email: 'aleks@tatoojmb.info', password: password, password_confirmation: password)

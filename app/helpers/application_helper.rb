dd45d61f86bf0f75d1da2736a7e40a0c355795dc
da <da@localhost.localdomain>
2019-10-02 12:04:19 +0300

    Add selection by price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     val = "plural.#{value}"
     count.to_s + ' ' + Russian.p(count, t("#{val}_1", locale: locale), t("#{val}_2", locale: locale), t("#{val}_10", locale: locale))
   end
+
+  def check_price_params(params, price)
+    (params && params[:price_between].present?) && params[:price_between] == "#{price[0]}..#{price[1]}"
+  end
 end
